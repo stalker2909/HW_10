334b37a38afbdca2d037dfce7f33c47a47cb07fd
stalker2909 <b-2909@yandex.ru>
2022-04-03 23:39:45 +0300

    Ver1

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import utils
+
+
+app = Flask(__name__)
+candidates = utils.load_candidates()
+
+@app.route('/')
+def home():
+    str = '<pre>'
+
+    for candidate in candidates.values():
+        str += f'Имя кандидата - {candidate["name"]} <br>Позиция кандидата {candidate["id"]} <br>Навыки через запятую {candidate["skills"]} <br><br>'
+
+    str += '</pre>'
+    return str
+
+
+@app.route('/candidates/<int:id>')
+def profile(id):
+    candidate = candidates[id]
+
+    str = f'<img src={candidate["picture"]}></img> <br><br>Имя кандидата - {candidate["name"]} <br>Позиция кандидата {candidate["id"]} <br>Навыки через запятую {candidate["skills"]} <br><br>'
+
+    return str
+
+
+@app.route('/skill/<skill>')
+def search_skill(skill):
+
+    for candidate in candidates.values():
+        skills = candidate['skills'].split(', ')
+        skills = [x.lower() for x in skills]
+        if skill.lower() in skills:
+            str = f'Имя кандидата - {candidate["name"]} <br>Позиция кандидата {candidate["id"]} <br>Навыки через запятую {candidate["skills"]} <br><br>'
+
+    return str
+
+
+app.run()
---
+++ utils.py
@@ -0,0 +1,16 @@
+# from base64 import encode
+import json
+
+
+def load_candidates():
+    with open('candidates.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        candidates = {}
+
+        for i in data:
+            candidates[i['id']] = i
+
+        return candidates
+
+
+load_candidates()
